66c2664de696e3262992b58442758f0292038a7f
dpi <pro@danielph.in>
2025-10-23 08:53:02 +0800

    ✨Make `InPageAlert` a collection (#143)

--- Component/InPageAlert/InPageAlert.php
+++ Component/InPageAlert/InPageAlert.php
@@ -9,7 +9,11 @@ use Pinto\Slots;
 use PreviousNext\Ds\Common\Atom;
 use PreviousNext\Ds\Common\Utility;
 use PreviousNext\IdsTools\Scenario\Scenarios;
+use Ramsey\Collection\AbstractCollection;
 
+/**
+ * @extends AbstractCollection<mixed>
+ */
 #[ObjectType\Slots(slots: [
   'heading',
   'type',
@@ -17,37 +21,49 @@ use PreviousNext\IdsTools\Scenario\Scenarios;
   'link',
 ])]
 #[Scenarios([InPageAlertScenarios::class])]
-class InPageAlert implements Utility\CommonObjectInterface {
+class InPageAlert extends AbstractCollection implements Utility\CommonObjectInterface {
 
   use Utility\ObjectTrait;
 
+  /**
+   * @phpstan-param Atom\Html\Html|iterable<mixed>|null $content
+   */
   final private function __construct(
     protected Atom\Heading\Heading $heading,
     protected Type $type,
-    protected Atom\Html\Html $content,
+    Atom\Html\Html|iterable|null $content,
     protected ?Atom\Link\Link $link,
   ) {
+    if ($content !== NULL) {
+      parent::__construct(\iterator_to_array($content));
+    }
   }
 
+  /**
+   * @phpstan-param Atom\Html\Html|iterable<mixed>|null $content
+   */
   public static function create(
     Atom\Heading\Heading $heading,
     Type $type,
-    Atom\Html\Html $content,
+    Atom\Html\Html|iterable|null $content = NULL,
     ?Atom\Link\Link $link = NULL,
   ): static {
     return static::factoryCreate(
-      $heading,
-      $type,
-      $content,
-      $link,
+      heading: $heading,
+      type: $type,
+      content: $content,
+      link: $link,
     );
+  }
+
+  public function getType(): string {
+    return 'mixed';
   }
 
   protected function build(Slots\Build $build): Slots\Build {
     return $build
       ->set('heading', $this->heading)
       ->set('type', $this->type->name)
-      ->set('content', $this->content)
       ->set('link', $this->link);
   }
 
--- Component/InPageAlert/InPageAlertScenarios.php
+++ Component/InPageAlert/InPageAlertScenarios.php
@@ -16,11 +16,24 @@ final class InPageAlertScenarios {
 
     /** @var InPageAlert $instance */
     $instance = InPageAlert::create(
-      CommonAtoms\Heading\Heading::create('Heading!', \PreviousNext\Ds\Common\Atom\Heading\HeadingLevel::Two),
+      CommonAtoms\Heading\Heading::create('Heading!', CommonAtoms\Heading\HeadingLevel::Two),
       Type::Success,
       CommonAtoms\Html\Html::create(Markup::create('<div>Foo <strong>bar</strong></div>')),
       CommonAtoms\Link\Link::create(title: 'A link!', url: $url),
     );
+    return $instance;
+  }
+
+  final public static function contentCollection(): InPageAlert {
+    $instance = InPageAlert::create(
+      CommonAtoms\Heading\Heading::create('Heading!', CommonAtoms\Heading\HeadingLevel::Two),
+      Type::Success,
+      CommonAtoms\Html\Html::create(Markup::create('<p>Item 0.</p>')),
+    );
+    $instance[] = CommonAtoms\Html\Html::create(Markup::create(<<<MARKUP
+      <p>Item 1.</p>
+      MARKUP));
+    $instance[] = CommonAtoms\Button\Button::create(title: 'Item 2', as: CommonAtoms\Button\ButtonType::Link);
     return $instance;
   }
 
