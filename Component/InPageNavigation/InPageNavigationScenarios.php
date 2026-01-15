d8fab8f62f8b5a7ea112885073979cf1e0859fdc
Michael Strelan <michael.strelan@previousnext.com.au>
2026-01-15 17:31:16 +1000

    PNXI-443: Add scenarios for `InPageNavigation` (#169)

--- Component/InPageNavigation/InPageNavigationScenarios.php
+++ Component/InPageNavigation/InPageNavigationScenarios.php
@@ -1,35 +1,52 @@
 <?php
 
-// @codingStandardsIgnoreStart
-
 declare(strict_types=1);
 
 namespace PreviousNext\Ds\Common\Component\InPageNavigation;
 
+use Drupal\Core\Render\Markup;
 use PreviousNext\Ds\Common\Atom;
+use PreviousNext\Ds\Common\Component as CommonComponents;
+use PreviousNext\Ds\Common\Layout\Section\Section;
+use PreviousNext\Ds\Common\Layout\Section\SectionType;
+use PreviousNext\IdsTools\Scenario\ScenarioSubject;
 
 final class InPageNavigationScenarios {
-// This scenario doesn't work in `ids` command yet due to library/chunk problems.
-//  final public static function standard(): InPageNavigation {
-//    /** @var \PreviousNext\Ds\Common\Component\InPageNavigation\InPageNavigation $instance */
-//    $instance = InPageNavigation::create(
-//      heading: Atom\Heading\Heading::create('Example!'),
-//    );
-//    $instance->containerAttributes['hello'] = 'world';
-//    $instance->containerAttributes['class'][] = 'foo';
-//    $instance->modifiers[] = IncludeHeadingLevels::H1;
-//    $instance->modifiers[] = IncludeHeadingLevels::H2;
-//    $instance->modifiers[] = IncludeHeadingLevels::H3;
-//
-//    // @todo need some demo content for this component to attach to.
-//    // $m = 0;
-//    // foreach (CommonComponents\InPageNavigation\IncludeHeadingLevels::cases() as $heading) {
-//    // for ($n = 1; $n <= 3; $n++) {
-//    // $m++;
-//    // $build[] = ['#markup' => sprintf('<%s id="heading-no%d">Test Heading (%s) #%d/%d</%s>', $heading->selector(), $m, $heading->selector(), $n, $m, $heading->selector())];
-//    // }
-//    // }
-//    return $instance;
-//  }
+
+  final public static function inPageNavigation(): ScenarioSubject {
+    $section = Section::create(
+      as: SectionType::Div,
+      heading: 'In page nav container',
+    );
+
+    $instance = InPageNavigation::create(
+      heading: Atom\Heading\Heading::create('Example!', Atom\Heading\HeadingLevel::Two),
+    );
+    $instance->containerAttributes['hello'] = 'world';
+    $instance->containerAttributes['class'][] = 'foo';
+    $instance->modifiers[] = IncludeHeadingLevels::H1;
+    $instance->modifiers[] = IncludeHeadingLevels::H2;
+    $instance->modifiers[] = IncludeHeadingLevels::H3;
+
+    $section[] = $instance;
+
+    $content = Section::create(
+      as: SectionType::Div,
+      isContainer: FALSE,
+    );
+    $content->containerAttributes['class'][] = 'js-content';
+
+    $m = 0;
+    for ($n = 1; $n <= 3; $n++) {
+      foreach (CommonComponents\InPageNavigation\IncludeHeadingLevels::cases() as $heading) {
+        $m++;
+        $content[] = Atom\Html\Html::create(Markup::create(\sprintf('<%s id="heading-no%d">Test Heading (%s) #%d/%d</%s>', $heading->selector(), $m, $heading->selector(), $n, $m, $heading->selector())));
+        $content[] = Atom\Html\Html::create(Markup::create('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.</p>'));
+      }
+    }
+
+    $section[] = $content;
+    return ScenarioSubject::createFromWiderContext($instance, $section);
+  }
 
 }
